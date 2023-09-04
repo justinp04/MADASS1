eb3461400695dddb4e22ea7a3c569e43759bf211
19766375 <19766375@student.curtin.edu.au>
2023-09-04 11:17:23 +0800

    Create PlayerState interface
    made an interface and children states.

---
+++ app/src/main/java/com/example/tictactoemad_20732435/AIPlayerState.java
@@ -0,0 +1,52 @@
+package com.example.tictactoemad_20732435;
+
+import android.widget.Button;
+
+import java.util.Random;
+
+public class AIPlayerState implements PlayerState
+{
+    @Override
+    public int playerTwoMove(Button[][] gameBoard)
+    {
+        try
+        {
+            Thread.sleep(500);
+        }
+        catch (InterruptedException e)
+        {
+            //Do nothing
+        }
+        finally
+        {
+            Button tile = generateRandomTile(gameBoard);
+            tile.setText("O");
+            return 1;
+        }
+    }
+
+    private Button generateRandomTile(Button[][] gameBoard)
+    {
+        Random rand = new Random();
+        Button tile = null;
+        int boardLength = gameBoard.length;
+        boolean emptyTile = false;
+        do
+        {
+            int horizPoint = rand.nextInt(boardLength);
+            int vertPoint = rand.nextInt(boardLength);
+            tile = gameBoard[horizPoint][vertPoint];
+
+            if (tile.getText().toString().equals(""))
+                emptyTile = true;
+        }while (emptyTile == false);
+
+        return tile;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "AI";
+    }
+}
